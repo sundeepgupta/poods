3ba3ab8c0d7de07ae8f76d443f55ba3e5cc67831
Sundeep Gupta <sundeep@sundeepgupta.ca>
2015-01-24 14:08:19 -0500

    Shameless green.

--- poods/Bottles.swift
+++ poods/Bottles.swift
@@ -3,14 +3,30 @@ import Foundation
 struct Bottles {
     
     func song() -> String {
-        return ""
+        return verses(high: 99, low: 0)
     }
     
-    func verses(#x: Int, y: Int) -> String {
-        return ""
+    func verses(#high: Int, low: Int) -> String {
+        var verses = [String]()
+        let range = reverse(low...high)
+        for i in range {
+            verses.append(verse(i))
+        }
+        
+        let joiner = "\n\n"
+        return joiner.join(verses)
     }
     
     func verse(number: Int) -> String {
-        return ""
+        switch number {
+        case 0:
+            return "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall."
+        case 1:
+            return "\(number) bottle of beer on the wall, \(number) bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall."
+        case 2:
+            return "\(number) bottles of beer on the wall, \(number) bottles of beer.\nTake one down and pass it around, \(number-1) bottle of beer on the wall."
+        default:
+            return "\(number) bottles of beer on the wall, \(number) bottles of beer.\nTake one down and pass it around, \(number-1) bottles of beer on the wall."
+        }
     }
 }
--- poodsTests/bottlesTest.swift
+++ poodsTests/bottlesTest.swift
@@ -2,58 +2,67 @@ import XCTest
 
 class bottlesTest: XCTestCase {
     
-    func ___testFirstVerse() {
+    func testFirstVerse() {
         let bottles = Bottles()
 
         let expected = "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall."
         
-        XCTAssertEqual(expected, bottles.verse(99), "pass")
+        XCTAssertEqual(expected, bottles.verse(99))
     }
     
-    func ___testAnotherVerse() {
+    func testAnotherVerse() {
         let bottles = Bottles()
         
         let expected = "89 bottles of beer on the wall, 89 bottles of beer.\nTake one down and pass it around, 88 bottles of beer on the wall."
         
-        XCTAssertEqual(expected, bottles.verse(89), "pass")
+        XCTAssertEqual(expected, bottles.verse(89))
     }
     
-    func ___testVerse2() {
+    func testVerse2() {
         let bottles = Bottles()
         
         let expected = "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall."
         
-        XCTAssertEqual(expected, bottles.verse(2), "pass")
+        XCTAssertEqual(expected, bottles.verse(2))
     }
     
-    func ___testVerse1() {
+    func testVerse1() {
         let bottles = Bottles()
         
         let expected = "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall."
         
-        XCTAssertEqual(expected, bottles.verse(1), "pass")
+        XCTAssertEqual(expected, bottles.verse(1))
     }
     
-    func ___testVerse0() {
+    func testVerse0() {
         let bottles = Bottles()
         
         let expected = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall."
         
-        XCTAssertEqual(expected, bottles.verse(0), "pass")
+        XCTAssertEqual(expected, bottles.verse(0))
     }
     
-    func ___testFewVerses() {
+    func testCoupleVerses() {
+        let bottles = Bottles()
+        
+        let expected = "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n\n98 bottles of beer on the wall, 98 bottles of beer.\nTake one down and pass it around, 97 bottles of beer on the wall."
+        
+        XCTAssertEqual(expected, bottles.verses(high: 99, low: 98))
+    }
+
+    
+    func testFewVerses() {
         let bottles = Bottles()
         
         let expected = "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n\n1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall."
         
-        XCTAssertEqual(expected, bottles.verses(x: 2, y: 0), "pass")
+        XCTAssertEqual(expected, bottles.verses(high: 2, low: 0))
     }
     
-    func ___testWholeSong() {
+    func testWholeSong() {
         let bottles = Bottles()
         
-        XCTAssertEqual(bottles.verses(x: 99, y: 0), bottles.song(), "pass")
+        XCTAssertEqual(bottles.verses(high: 99, low: 0), bottles.song())
     }
     
     

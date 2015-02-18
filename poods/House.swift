9ab3f39fac9dbcffa8f71c5d2d63a4df58e46f66
Sundeep Gupta <sundeep@sundeepgupta.ca>
2015-02-18 05:24:47 -0500

    DRY shameless green

--- poods/House.swift
+++ poods/House.swift
@@ -1,29 +1,31 @@
 class House {
-    
-    let lines = ["This is the house that Jack built.\n",
-                "This is the malt that lay in the house that Jack built.\n",
-                "This is the rat that ate the malt that lay in the house that Jack built.\n",
-                "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-                "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-                "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-                "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-                "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-                "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-                "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-                "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n",
-                "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"]
-    
+    let phrases = [ "the house that Jack built.\n",
+                    "the malt that lay in",
+                    "the rat that ate",
+                    "the cat that killed",
+                    "the dog that worried",
+                    "the cow with the crumpled horn that tossed",
+                    "the maiden all forlorn that milked",
+                    "the man all tattered and torn that kissed",
+                    "the priest all shaven and shorn that married",
+                    "the rooster that crowed in the morn that woke",
+                    "the farmer sowing his corn that kept",
+                    "the horse and the hound and the horn that belonged to" ]
     
     func recite() -> String {
+        var lines = [String]()
+        for i in 1...self.phrases.count {
+            lines.append(self.line(i))
+        }
+        
         let joiner = "\n"
-        return joiner.join(self.lines)
+        return joiner.join(lines)
     }
     
     func line(number: Int) -> String {
-        if number <= self.lines.count {
-            return self.lines[number - 1]
-        } else {
-            return ""
-        }
+        var linePhrases = self.phrases[0...number-1].reverse()
+        linePhrases.insert("This is", atIndex: 0)
+        let joiner = " "
+        return joiner.join(linePhrases)
     }
 }

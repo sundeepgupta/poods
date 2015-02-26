778bd538a1a55dfbd84251cd94b80b6b0288a8a4
Sundeep Gupta <sundeep@sundeepgupta.ca>
2015-02-26 08:35:14 -0500

    Add randomness

--- poods/House.swift
+++ poods/House.swift
@@ -1,31 +1,69 @@
+import Foundation
+
 class House {
-    let phrases = [ "the house that Jack built.\n",
-                    "the malt that lay in",
-                    "the rat that ate",
-                    "the cat that killed",
-                    "the dog that worried",
-                    "the cow with the crumpled horn that tossed",
-                    "the maiden all forlorn that milked",
-                    "the man all tattered and torn that kissed",
-                    "the priest all shaven and shorn that married",
-                    "the rooster that crowed in the morn that woke",
-                    "the farmer sowing his corn that kept",
-                    "the horse and the hound and the horn that belonged to" ]
+    let lineStart = "This is"
+    let innerPhrases = ["the malt that lay in",
+        "the rat that ate",
+        "the cat that killed",
+        "the dog that worried",
+        "the cow with the crumpled horn that tossed",
+        "the maiden all forlorn that milked",
+        "the man all tattered and torn that kissed",
+        "the priest all shaven and shorn that married",
+        "the rooster that crowed in the morn that woke",
+        "the farmer sowing his corn that kept",
+        "the horse and the hound and the horn that belonged to"]
+    let endPhrase = "the house that Jack built.\n"
+    let shuffler: Shuffler
+    
+    init(shuffler: Shuffler = Shuffler()) {
+        self.shuffler = shuffler
+    }
     
     func recite() -> String {
         var lines = [String]()
-        for i in 1...self.phrases.count {
+        for i in 1...self.innerPhrases.count+1 {
             lines.append(self.line(i))
         }
         
-        let joiner = "\n"
-        return joiner.join(lines)
+        return "\n".join(lines)
     }
     
     func line(number: Int) -> String {
-        var linePhrases = self.phrases[0...number-1].reverse()
-        linePhrases.insert("This is", atIndex: 0)
-        let joiner = " "
-        return joiner.join(linePhrases)
+        return self.line(number, phrases: self.innerPhrases)
+    }
+    
+    func randomRecite() -> String {
+        var lines = [String]()
+        let phrases = self.shuffler.list(self.innerPhrases)
+        for i in 1...self.innerPhrases.count+1 {
+            lines.append(self.line(i, phrases: phrases))
+        }
+        
+        return "\n".join(lines)
+    }
+    
+    func randomLine(number: Int) -> String {
+        var shuffled = self.shuffler.list(self.innerPhrases)
+        return self.line(number, phrases: shuffled)
+    }
+    
+    private func line(number: Int, var phrases: Array<String>) -> String {
+        phrases.insert(self.endPhrase, atIndex: 0)
+        var linePhrases = phrases[0...number-1].reverse()
+        linePhrases.insert(self.lineStart, atIndex: 0)
+        return " ".join(linePhrases)
+    }
+    
+}
+
+class Shuffler {
+    func list(var list: Array<String>) -> Array<String> {
+        let listCount = count(list)
+        for i in 0..<(listCount - 1) {
+            let j = Int(arc4random_uniform(UInt32(listCount - i))) + i
+            swap(&list[i], &list[j])
+        }
+        return list
     }
 }
--- poodsTests/HouseTest.swift
+++ poodsTests/HouseTest.swift
@@ -1,5 +1,21 @@
 import XCTest
 
+class MockShuffler : Shuffler {
+    override func list(var list: Array<String>) -> Array<String> {
+        return ["the dog that worried",
+            "the malt that lay in",
+            "the maiden all forlorn that milked",
+            "the rat that ate",
+            "the horse and the hound and the horn that belonged to",
+            "the man all tattered and torn that kissed",
+            "the cat that killed",
+            "the farmer sowing his corn that kept",
+            "the rooster that crowed in the morn that woke",
+            "the priest all shaven and shorn that married",
+            "the cow with the crumpled horn that tossed"]
+    }
+}
+
 class HouseTest : XCTestCase {
     func testLine1() {
         let tale = House()
@@ -76,18 +92,52 @@ class HouseTest : XCTestCase {
     func testAllLines() {
         let tale = House()
         let expected = "This is the house that Jack built.\n\n" +
-                        "This is the malt that lay in the house that Jack built.\n\n" +
-                        "This is the rat that ate the malt that lay in the house that Jack built.\n\n" +
-                        "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n" +
-                        "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n" +
-                        "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n" +
-                        "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n" +
-                        "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n" +
-                        "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n" +
-                        "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n" +
-                        "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n" +
-                    "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
+            "This is the malt that lay in the house that Jack built.\n\n" +
+            "This is the rat that ate the malt that lay in the house that Jack built.\n\n" +
+            "This is the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n" +
+            "This is the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n" +
+            "This is the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n" +
+            "This is the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n" +
+            "This is the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n" +
+            "This is the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n" +
+            "This is the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n" +
+            "This is the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n\n" +
+            "This is the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in the house that Jack built.\n"
         XCTAssertEqual(expected, tale.recite())
     }
-
+    
+    func testRandomLine1() {
+        let tale = House(shuffler: MockShuffler())
+        let expected = "This is the house that Jack built.\n"
+        XCTAssertEqual(expected, tale.randomLine(1))
+    }
+    
+    func testRandomLine2() {
+        let tale = House(shuffler: MockShuffler())
+        let expected = "This is the dog that worried the house that Jack built.\n"
+        XCTAssertEqual(expected, tale.randomLine(2))
+    }
+    
+    func testRandomLine3() {
+        let tale = House(shuffler: MockShuffler())
+        let expected = "This is the malt that lay in the dog that worried the house that Jack built.\n"
+        XCTAssertEqual(expected, tale.randomLine(3))
+    }
+    
+    func testRandomAllLines() {
+        let tale = House(shuffler: MockShuffler())
+        let expected = "This is the house that Jack built.\n\n" +
+            "This is the dog that worried the house that Jack built.\n\n" +
+            "This is the malt that lay in the dog that worried the house that Jack built.\n\n" +
+            "This is the maiden all forlorn that milked the malt that lay in the dog that worried the house that Jack built.\n\n" +
+            "This is the rat that ate the maiden all forlorn that milked the malt that lay in the dog that worried the house that Jack built.\n\n" +
+            "This is the horse and the hound and the horn that belonged to the rat that ate the maiden all forlorn that milked the malt that lay in the dog that worried the house that Jack built.\n\n" +
+            "This is the man all tattered and torn that kissed the horse and the hound and the horn that belonged to the rat that ate the maiden all forlorn that milked the malt that lay in the dog that worried the house that Jack built.\n\n" +
+            "This is the cat that killed the man all tattered and torn that kissed the horse and the hound and the horn that belonged to the rat that ate the maiden all forlorn that milked the malt that lay in the dog that worried the house that Jack built.\n\n" +
+            "This is the farmer sowing his corn that kept the cat that killed the man all tattered and torn that kissed the horse and the hound and the horn that belonged to the rat that ate the maiden all forlorn that milked the malt that lay in the dog that worried the house that Jack built.\n\n" +
+            "This is the rooster that crowed in the morn that woke the farmer sowing his corn that kept the cat that killed the man all tattered and torn that kissed the horse and the hound and the horn that belonged to the rat that ate the maiden all forlorn that milked the malt that lay in the dog that worried the house that Jack built.\n\n" +
+            "This is the priest all shaven and shorn that married the rooster that crowed in the morn that woke the farmer sowing his corn that kept the cat that killed the man all tattered and torn that kissed the horse and the hound and the horn that belonged to the rat that ate the maiden all forlorn that milked the malt that lay in the dog that worried the house that Jack built.\n\n" +
+            "This is the cow with the crumpled horn that tossed the priest all shaven and shorn that married the rooster that crowed in the morn that woke the farmer sowing his corn that kept the cat that killed the man all tattered and torn that kissed the horse and the hound and the horn that belonged to the rat that ate the maiden all forlorn that milked the malt that lay in the dog that worried the house that Jack built.\n"
+        XCTAssertEqual(expected, tale.randomRecite())
+    }
 }
